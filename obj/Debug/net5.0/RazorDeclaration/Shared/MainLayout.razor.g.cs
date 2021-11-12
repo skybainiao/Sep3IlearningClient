00815bf75b0b311740b17637335926a5bc5405a9
Chenben Tong <305594@via.dk>
2021-11-12 04:40:22 +0100

    asd

--- obj/Debug/net5.0/RazorDeclaration/Shared/MainLayout.razor.g.cs
+++ obj/Debug/net5.0/RazorDeclaration/Shared/MainLayout.razor.g.cs
@@ -13,77 +13,77 @@ namespace LoginExample.Shared
     using System.Threading.Tasks;
     using Microsoft.AspNetCore.Components;
 #nullable restore
-#line 1 "C:\Users\45527\Desktop\Sep3Project\Sep3Client\_Imports.razor"
+#line 1 "D:\JetBrainsRider\Sep3IlearningClient\_Imports.razor"
 using System.Net.Http;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 2 "C:\Users\45527\Desktop\Sep3Project\Sep3Client\_Imports.razor"
+#line 2 "D:\JetBrainsRider\Sep3IlearningClient\_Imports.razor"
 using Microsoft.AspNetCore.Authorization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 3 "C:\Users\45527\Desktop\Sep3Project\Sep3Client\_Imports.razor"
+#line 3 "D:\JetBrainsRider\Sep3IlearningClient\_Imports.razor"
 using Microsoft.AspNetCore.Components.Authorization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 4 "C:\Users\45527\Desktop\Sep3Project\Sep3Client\_Imports.razor"
+#line 4 "D:\JetBrainsRider\Sep3IlearningClient\_Imports.razor"
 using Microsoft.AspNetCore.Components.Forms;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 5 "C:\Users\45527\Desktop\Sep3Project\Sep3Client\_Imports.razor"
+#line 5 "D:\JetBrainsRider\Sep3IlearningClient\_Imports.razor"
 using Microsoft.AspNetCore.Components.Routing;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 6 "C:\Users\45527\Desktop\Sep3Project\Sep3Client\_Imports.razor"
+#line 6 "D:\JetBrainsRider\Sep3IlearningClient\_Imports.razor"
 using Microsoft.AspNetCore.Components.Web;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 7 "C:\Users\45527\Desktop\Sep3Project\Sep3Client\_Imports.razor"
+#line 7 "D:\JetBrainsRider\Sep3IlearningClient\_Imports.razor"
 using Microsoft.JSInterop;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 8 "C:\Users\45527\Desktop\Sep3Project\Sep3Client\_Imports.razor"
+#line 8 "D:\JetBrainsRider\Sep3IlearningClient\_Imports.razor"
 using LoginExample;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 9 "C:\Users\45527\Desktop\Sep3Project\Sep3Client\_Imports.razor"
+#line 9 "D:\JetBrainsRider\Sep3IlearningClient\_Imports.razor"
 using LoginExample.Shared;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 1 "C:\Users\45527\Desktop\Sep3Project\Sep3Client\Shared\MainLayout.razor"
+#line 1 "D:\JetBrainsRider\Sep3IlearningClient\Shared\MainLayout.razor"
 using LoginComponent;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 2 "C:\Users\45527\Desktop\Sep3Project\Sep3Client\Shared\MainLayout.razor"
+#line 2 "D:\JetBrainsRider\Sep3IlearningClient\Shared\MainLayout.razor"
 using LoginExample.Authentication;
 
 #line default
@@ -97,7 +97,7 @@ using LoginExample.Authentication;
         }
         #pragma warning restore 1998
 #nullable restore
-#line 23 "C:\Users\45527\Desktop\Sep3Project\Sep3Client\Shared\MainLayout.razor"
+#line 24 "D:\JetBrainsRider\Sep3IlearningClient\Shared\MainLayout.razor"
        
 
     [CascadingParameter] protected Task<AuthenticationState> AuthStat { get; set; }
