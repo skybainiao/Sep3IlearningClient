f1a1efcbae40b37cf74498f654871c91892cc489
Chen <chen2468830035@gmail.com>
2021-11-18 06:44:52 +0100

    up

--- Pages/home.js
+++ Pages/home.js
@@ -1,21 +1,12 @@
 
-    function myFunction(id) {
+function myFunction(id) {
     var x = document.getElementById(id);
     if (x.className.indexOf("w3-show") == -1) {
-    x.className += " w3-show";
-} else {
-    x.className = x.className.replace(" w3-show", "");
+        x.className += " w3-show";
+    } else {
+        x.className = x.className.replace(" w3-show", "");
+    }
 }
-}
-
-
-    $("[name='sexRadio']:eq(0)").parent("span").addClass("checked");
-    $("[name='sexRadio']:eq(0)").prop("checked",true);
 
 
 
-    function getRadio(){
-    var c = $('input:radio[name="sexRadio"]:checked').val();
-    var b = $('input[name="sexRadio"]:checked').val();
-    alert(c);
-}
