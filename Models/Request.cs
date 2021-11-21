97bfb62e900ccab78468b1dc209fbde12000a7b2
Chen <chen2468830035@gmail.com>
2021-11-21 03:57:14 +0100

    update

---
+++ Models/Request.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace LoginExample.Models
+{
+    public class Request
+    {
+        private String sender { set; get; }
+        private String receiver{ set; get; }
+        private String comment{ set; get; }
+    }
+}
