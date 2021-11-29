730e98bd40075180bf3d92d8c313a6d35e336d57
Chen <chen2468830035@gmail.com>
2021-11-29 04:29:40 +0100

    update

--- Models/Course.cs
+++ Models/Course.cs
@@ -3,5 +3,15 @@
     public class Course
     {
         
+        public string courseName { set; get; }
+        
+        public string session{ set; get; }
+        
+        public string data{ set; get; }
+        
+        public string preparation{ set; get; }
+        
+        public string content{ set; get; }
+        
     }
 }
